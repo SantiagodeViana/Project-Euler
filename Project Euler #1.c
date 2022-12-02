e37b8e976bc8b0235b46a38b998c8ccee95d1956
SantiagodeViana <68198881+SantiagodeViana@users.noreply.github.com>
2022-12-02 23:32:09 +0100

    Project Euler #1

---
+++ Project Euler #1.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    /* Solución para el problema #1 de Project Euler
+     * https://projecteuler.net/problem=1            */
+    int suma = 0;
+    for (int i = 0; i < 1000; i++){
+        if (i % 3 == 0|| i % 5 == 0) suma += i;
+        if (i == 9) printf("Suma de multiplos de 3 o 5 menores que 10: %d\n", suma);
+    }
+    printf("La suma de multiplos de 3 o 5 menores que 1000 es: %d\n", suma);
+    return 0;
+}
