83355ff75f804ee2dd9828605bd17f55c6e9f724
Santiago Viana <68198881+SantiagodeViana@users.noreply.github.com>
2022-12-09 11:02:26 +0100

    Add files via upload

---
+++ ProjectEuler82/Solucion.java
@@ -0,0 +1,12 @@
+public class Solucion {
+    private int solucion;
+    public Solucion(int solucion) {
+        this.solucion = solucion;
+    }
+    public int getSolucion() {
+        return this.solucion;
+    }
+    public void setSolucion(int solucion) {
+        this.solucion = solucion;
+    }
+}
