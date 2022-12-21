7270377584309364e0462a37705dffd34448dbe7
Santiago Viana <68198881+SantiagodeViana@users.noreply.github.com>
2022-12-21 16:18:59 +0100

    Create Main.java
    Solución para el problema #68 de Project Euler.

---
+++ ProjectEuler68 (No terminado)/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+       /* Solución para el problema #68 de Project Euler
+        * "Utilizando los números del 1 al 10, y dependiendo de los arreglos, es posible formar cadenas de 16 y 17 cifras. 
+        *  ¿Cuál es la cadena máxima de 16 dígitos para un anillo "mágico" de 5 gones?"
+        * https://projecteuler.net/problem=68                                             */
+     
+        Gon.setValor(5);
+        System.out.println(Gon.getValor());
+    }
+}
