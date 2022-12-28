2b39e1d027948ac4dcbef6db6840c28d33fdc3d5
Santiago Viana <68198881+SantiagodeViana@users.noreply.github.com>
2022-12-28 11:04:07 +0100

    Create Project Euler #7.c

---
+++ ProjectEuler07/Project Euler #7.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+int esPrimo(int n);
+
+int main()
+{
+    int n = 1, respuesta = 1;
+    while (respuesta <= 10001){
+        n++;
+        if (esPrimo(n)) respuesta++;
+    }
+    printf("\nLa respuesta es %d\n", n);
+    return 0;
+}
+
+int esPrimo(int n){ //Función para determinar si un numero es primo, basada en la Criba de Eratóstenes
+    if (n == 1) return 1; //Cambiar a 0 o mantener a conveniencia del programa
+    else if (n < 4) return 1; //Si son menores de 4, son primos
+    else if (n % 2 == 0) return 0; //Si son pares, no son primos
+    else if (n < 9) return 1; //Si son menores de 9 e impares, son primos
+    else if (n % 3 == 0) return 0; //Si son múltiplos de 3, no son primos
+    else{
+        int r = floor(sqrt(n));
+        int f = 5;
+        while (f <= r){
+            if (n % f == 0) return 0;
+            else if (n % (f + 2) == 0) return 0;
+            else f += 6;
+        }
+        return 1;
+    }
+}
