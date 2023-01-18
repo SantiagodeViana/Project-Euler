43b0ce9d4461f0f4c72ab0df7f779f2881fc4bf8
Santiago Viana <68198881+SantiagodeViana@users.noreply.github.com>
2023-01-18 15:33:04 +0100

    Rename esPrimo.c to Funciones/esPrimo.c

--- Funciones/esPrimo.c
+++ Funciones/esPrimo.c
@@ -1,9 +1,9 @@
-int esPrimo(int n){//Función para determinar si un numero es primo, basada en la Criba de Eratóstenes
+int esPrimo(int n){//FunciÃ³n para determinar si un numero es primo, basada en la Criba de EratÃ³stenes
     if (n == 1) return 1; ////*Cambiar a 0 o mantener a conveniencia del programa
     else if (n < 4) return 1; //Si son menores de 4, son primos
     else if (n % 2 == 0) return 0; //Si son pares, no son primos
     else if (n < 9) return 1; //Si son menores de 9 e impares, son primos
-    else if (n % 3 == 0) return 0; //Si son múltiplos de 3, no son primos
+    else if (n % 3 == 0) return 0; //Si son mÃºltiplos de 3, no son primos
     else{
         int r = floor(sqrt(n));
         int f = 5;
