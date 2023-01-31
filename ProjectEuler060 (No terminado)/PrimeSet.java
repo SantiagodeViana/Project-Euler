cca8204e15fd750a7e30c3121cac0aa06608bc54
Santiago Viana <68198881+SantiagodeViana@users.noreply.github.com>
2023-01-31 20:43:52 +0100

    Update PrimeSet.java
    Revirtiendo a Criba de Eratóstenes

--- ProjectEuler060 (No terminado)/PrimeSet.java
+++ ProjectEuler060 (No terminado)/PrimeSet.java
@@ -1,5 +1,5 @@
 public class PrimeSet {
-    public static boolean concatenatedPairSet(int[] primos, int n1, int n2, int n3, int n4, int n5, int N){
+    public static boolean concatenatedPairSet(int n1, int n2, int n3, int n4, int n5){
         boolean pairSet = true;
         int cifras = 10, aux, cadena, i = 0, j;
         int [] set= {n1, n2, n3, n4, n5};
@@ -10,13 +10,13 @@ public class PrimeSet {
                 aux = set[j] * cifras;
                 cadena = aux + set[i];
                 //System.out.println(cadena);
-                if (busquedaBinaria(primos, cadena, N)) { //Concadenando desde la izquierda
+                if (esPrimo(cadena)) { //Concadenando desde la izquierda
                     cifras = 10; //Reiniciando contador de cifras
                     while (cifras < set[j]) cifras *= 10;
                     aux = set[i] * cifras;
                     cadena = aux + set[j];
                     //System.out.println(cadena);
-                    if (!busquedaBinaria(primos, cadena, N)) return false;
+                    if (esPrimo(cadena)) return false;
                 }
                 else return false;
                 cifras = 10; //Reiniciando contador de cifras
@@ -27,19 +27,18 @@ public class PrimeSet {
         return pairSet;
     }
 
-    public static boolean busquedaBinaria(int[] primos, int n, int N){ //Determina si un elemento se encuentra en un array mediante una búsqueda binaria
-        //Requiere como entrada un array ordenado
-        if (n < 0 || n > primos[N-1]) return false; //Rechaza números negativos y superiores al último elemento del array
+    public static boolean busquedaBinaria(int[] primos, int n, int N){
+        if (n < 0 || n > primos[N-1]) return false;
         else return busquedaBinariaAux(primos, n, 0, N-1);
     }
-    
+
     public static boolean busquedaBinariaAux(int[] primos, int n, int io, int iN){
         boolean encontrado = false;
         int k = (iN+io) / 2;
         if (io == iN){
             if (n != primos[k]) return false;
-            else return true;
         }
+        if (n == primos[k]) return true;
         else{
             while (!encontrado && io != iN){
                 if (n <= primos[k]) return busquedaBinariaAux(primos, n, io, k);
@@ -50,7 +49,7 @@ public class PrimeSet {
     }
 
     public static boolean esPrimo(int n){//Función para determinar si un número es primo, basada en la Criba de Eratóstenes
-        if (n == 1) return true; ////*Cambiar a 0 o mantener a conveniencia del programa
+        if (n == 1) return true; //Cambiar a 0 o mantener a conveniencia del programa
         else if (n < 4) return true; //Si son menores de 4, son primos
         else if (n % 2 == 0) return false; //Si son pares, no son primos
         else if (n < 9) return true; //Si son menores de 9 e impares, son primos
