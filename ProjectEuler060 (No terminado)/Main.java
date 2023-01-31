dd61469397438330e1e4c98ff04b7ab32e0211c7
Santiago Viana <68198881+SantiagodeViana@users.noreply.github.com>
2023-01-31 17:48:21 +0100

    Update Main.java
    Arreglos y comentarios

--- ProjectEuler060 (No terminado)/Main.java
+++ ProjectEuler060 (No terminado)/Main.java
@@ -1,17 +1,17 @@
 public class Main {
     public static void main (String[] args){
         //Lista de primos de hasta 8 dígitos; permitirá un acceso estático para mejorar la rapidez del programa
-        int [] primos = new int[6000000];
+        int [] primos = new int[100000000];
         int respuesta = Integer.MAX_VALUE, p1 = 0, p2 = 0, p3 = 0, p4 = 0, p5 = 0, N = 0;
-        for (int i = 0; i <= 100000000; i++){ //Rellenando lista
+        for (int i = 2; i <= 100000000; i++){ //Rellenando lista
             if (PrimeSet.esPrimo(i)){
                 primos[N] = i;
                 N++;
             }
-            if (i % 1000000 == 0) System.out.println("i: " + i);
+            if (i % 1000000 == 0) System.out.println("Rellenando array de primos (" + i/1000000 + "%)");
         }
-        System.out.println("Array rellenado"); //1229 primos?
-        for (int a = 0; a < 1000; a++){ //7-1000
+        System.out.println("Array rellenado"); //N=5761457 
+        for (int a = 0; a < 1000; a++){ //Último primo de 4 dígitos en i: 1230; 7-1000 
             System.out.println("a= " + a);
             for (int b = a+1; b < 1000; b++){
                 for (int c = b+1; c < 1000; c++){
