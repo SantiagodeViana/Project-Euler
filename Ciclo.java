eb46056a490cfee3b742d467ff0e0bd0ff5fd14e
Santiago Viana <68198881+SantiagodeViana@users.noreply.github.com>
2023-01-31 20:44:33 +0100

    Create Ciclo.java

---
+++ Ciclo.java
@@ -0,0 +1,29 @@
+public class Ciclo {
+   /*public static int longitudCiclo(double n){
+        boolean repetido = false;
+        int i = 0;
+        int [] decimal = new int [10];
+        decimal[0] = -1;
+        while (decimal [i] != -1 && !repetido){
+            n *= 10.0
+        }
+    }*/
+
+    /*
+        double respuesta, aux;
+        respuesta = 1.0 / 7.0;
+        int N = 16;
+        int [] decimal = new int [N];
+        int i = 0;
+        aux = respuesta;
+        for (double j = 1.0; j < N; j++){
+            aux *= 10.0;
+            if (i != 0) aux -= decimal[i-1] * 10;
+            decimal[i] = (int) (aux % 10);
+            i++;
+            System.out.println("Fracción: " + aux);
+        }
+        System.out.print("Parte decimal: ");
+        for (i = 0; i < N; i++) System.out.print(decimal[i] + ", ");
+     */
+}
