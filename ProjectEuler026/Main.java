e6a55a3ffecbe1e428a71f87a6a86b25b15c7e09
Santiago Viana <68198881+SantiagodeViana@users.noreply.github.com>
2023-02-01 11:58:46 +0100

    Update Main.java
    Implementando Ciclo.longitudCiclo

--- ProjectEuler026/Main.java
+++ ProjectEuler026/Main.java
@@ -1,19 +1,7 @@
 public class Main {
     public static void main(String[] args) {
-        double respuesta, aux;
+        double respuesta;
         respuesta = 1.0 / 7.0;
-        int N = 16;
-        int [] decimal = new int [N];
-        int i = 0;
-        aux = respuesta;
-        for (double j = 1.0; j < N; j++){
-            aux *= 10.0;
-            if (i != 0) aux -= decimal[i-1] * 10;
-            decimal[i] = (int) (aux % 10);
-            i++;
-            System.out.println("Fracción: " + aux);
-        }
-        System.out.print("Parte decimal: ");
-        for (i = 0; i < N; i++) System.out.print(decimal[i] + ", ");
+        System.out.println("La longitud del ciclo es: " + Ciclo.longitudCiclo(respuesta));
     }
 }
