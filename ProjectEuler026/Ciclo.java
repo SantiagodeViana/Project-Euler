475c564c85038a9b9ec6a3b444e328ee73a9ef88
Santiago Viana <68198881+SantiagodeViana@users.noreply.github.com>
2023-02-01 13:28:52 +0100

    Update Ciclo.java
    Arreglando errores de funcionalidad. +comentarios.

--- ProjectEuler026/Ciclo.java
+++ ProjectEuler026/Ciclo.java
@@ -1,40 +1,31 @@
-public class Ciclo {
-   public static int longitudCiclo(double n){ //Método para determinar longitud de ciclo recurrente. Recibe double como entrada
-       boolean repetido = false;
-       int i = 0, N = 16, longitud = 0; //N: tamaño array; longitud: mide el ciclo;
-       int [] decimal = new int [N];
-       decimal[0] = -1;
-       double j = 1.0;
-       while (j < N && !repetido){
-           j++;
-           n *= 10.0;
-           if (i != 0) n -= decimal[i-1] * 10; //En caso de haber empezado el bucle, uno sólo se queda con una unidad
-           decimal[i] = (int) (n % 10); //Se adquiere un decimal al mismo tiempo
-           for (int k = 0; k <= i && !repetido; k++){ //Bucle para comprobar repeticiones en el ciclo
-               if (decimal[k] == decimal[i]) repetido = true;
-               else if (k == i) longitud++;
-           }
-           System.out.println("La longitud del ciclo es: " + Ciclo.longitudCiclo());
-           i++;
-       }
+public class Ciclo { //Actualmente mide a veces la longitud del ciclo con una unidad de exceso
+    public static int longitudCiclo(double n){ //Método para determinar longitud de ciclo recurrente. Recibe double como entrada
+        boolean repetido = false;
+        int i = 0, N = 16, longitud = 0, rep = 0; //N: tamaño array; longitud: mide el ciclo;
+        int [] decimal = new int [N];
+        decimal[0] = -1;
+        while (i < N && !repetido){
+            n *= 10.0;
+            if (i != 0) n -= decimal[i-1] * 10; //En caso de haber empezado el bucle, uno sólo se queda con una unidad
+            decimal[i] = (int) (n % 10); //Se adquiere un decimal al mismo tiempo
+            for (int j = 0; j <= i && !repetido; j++){ //Bucle para comprobar repeticiones en el ciclo
+                if (decimal[j] == decimal[i] && i != 0 && i != j){
+                    repetido = true;
+                    /* Este bucle confirma si la repetición es genuina
+                     * Si 5 dìgitos se repiten, o durante toda la longitud, se considera como la longitud final del ciclo */
+                    while (rep < 5 && i+rep < longitud && repetido){
+                        if (decimal[j+rep] != decimal[i+rep]) repetido = false;
+                        else rep++;
+                    }
+                    if (repetido == false){
+                        longitud +=rep;
+                        j += rep;
+                    }
+                }
+                else if (j == i && !repetido) longitud++;
+            }
+            i++;
+        }
         return longitud;
     }
-
-    /*
-        double respuesta, aux;
-        respuesta = 1.0 / 7.0;
-        int N = 16;
-        int [] decimal = new int [N];
-        int i = 0;
-        aux = respuesta;
-        for (double j = 1.0; j < N; j++){
-            aux *= 10.0;
-            if (i != 0) aux -= decimal[i-1] * 10;
-            decimal[i] = (int) (aux % 10);
-            i++;
-            System.out.println("Fracción: " + aux);
-        }
-        System.out.print("Parte decimal: ");
-        for (i = 0; i < N; i++) System.out.print(decimal[i] + ", ");
-     */
 }
