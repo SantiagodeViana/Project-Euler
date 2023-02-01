321c9d183064074ddcbf724a6dcf42823b52f8aa
Santiago Viana <68198881+SantiagodeViana@users.noreply.github.com>
2023-02-01 11:57:30 +0100

    Update Ciclo.java
    Implementando método longitudCiclo, implementando detector de repeticiones

--- ProjectEuler026/Ciclo.java
+++ ProjectEuler026/Ciclo.java
@@ -1,13 +1,24 @@
 public class Ciclo {
-   /*public static int longitudCiclo(double n){
-        boolean repetido = false;
-        int i = 0;
-        int [] decimal = new int [10];
-        decimal[0] = -1;
-        while (decimal [i] != -1 && !repetido){
-            n *= 10.0
-        }
-    }*/
+   public static int longitudCiclo(double n){ //Método para determinar longitud de ciclo recurrente. Recibe double como entrada
+       boolean repetido = false;
+       int i = 0, N = 16, longitud = 0; //N: tamaño array; longitud: mide el ciclo;
+       int [] decimal = new int [N];
+       decimal[0] = -1;
+       double j = 1.0;
+       while (j < N && !repetido){
+           j++;
+           n *= 10.0;
+           if (i != 0) n -= decimal[i-1] * 10; //En caso de haber empezado el bucle, uno sólo se queda con una unidad
+           decimal[i] = (int) (n % 10); //Se adquiere un decimal al mismo tiempo
+           for (int k = 0; k <= i && !repetido; k++){ //Bucle para comprobar repeticiones en el ciclo
+               if (decimal[k] == decimal[i]) repetido = true;
+               else if (k == i) longitud++;
+           }
+           System.out.println("La longitud del ciclo es: " + Ciclo.longitudCiclo());
+           i++;
+       }
+        return longitud;
+    }
 
     /*
         double respuesta, aux;
