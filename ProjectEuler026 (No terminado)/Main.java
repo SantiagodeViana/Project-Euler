be4816fcfc24b5acde76d1007513e91a7f1f085e
Santiago Viana <68198881+SantiagodeViana@users.noreply.github.com>
2023-02-02 11:41:52 +0100

    Update and rename ProjectEuler026 (No terminado)/Main.java to Main.java/ProjectEuler026_(No_terminado)

--- ProjectEuler026 (No terminado)/Main.java
+++
@@ -1,7 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        double respuesta;
-        respuesta = 1.0 / 7.0;
-        System.out.println("La longitud del ciclo es: " + Ciclo.longitudCiclo(respuesta));
-    }
-}
