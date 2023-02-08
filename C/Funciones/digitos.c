12a5dde7d78465900c321f5ccaa0e5c51adc7e1f
Santiago Viana <68198881+SantiagodeViana@users.noreply.github.com>
2023-02-09 00:06:23 +0100

    Create digitos.c

---
+++ C/Funciones/digitos.c
@@ -0,0 +1,10 @@
+//Función usada en Project Euler #59
+
+int digitos(int n){ //Cuenta los dígitos de una cifra
+    int respuesta = 0, i = 1;
+    while (i <= n){
+        i*=10;
+        respuesta++;
+    }
+    return respuesta;
+}
