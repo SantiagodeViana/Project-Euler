b64537e900c93766e24f869a443d74150eddc7c3
Santiago Viana <68198881+SantiagodeViana@users.noreply.github.com>
2023-02-10 10:47:03 +0100

    Update Project Euler #048.py
    Comentarios

--- Python/Project Euler #048.py
+++ Python/Project Euler #048.py
@@ -1,6 +1,10 @@
+# Solución para el problema #48 de Project Euler
+# "Encuentra los diez últimos dígitos de la serie, 1^1 + 2^2 + 3^3 + ... + 1000^1000."
+# https://projecteuler.net/problem=48
+
 base = 1
 suma = 0
-while base <= 10:
+while base <= 10: #Calcula la serie
 	suma += (base ** base)
 	base += 1
     	print base
