0d18f8be0e04e0775bb2f7bb7862487a63af8039
Santiago Viana <68198881+SantiagodeViana@users.noreply.github.com>
2023-02-10 10:17:40 +0100

    Update Project Euler #016.py
    Comentarios

--- Python/Project Euler #016.py
+++ Python/Project Euler #016.py
@@ -1,7 +1,11 @@
+# Solución para el problema #16 de Project Euler
+# ¿Cuál es la suma de los dígitos del número 2^1000?
+# https://projecteuler.net/problem=16
+
 def sum_digits(n):
-    s = 0
+    s = 0 #Variable suma
     while n:
-        s += n % 10
+        s += n % 10 #Se suman los digitos, uno por uno
         n //= 10
     return s
 
