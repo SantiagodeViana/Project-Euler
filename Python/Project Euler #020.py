36682fdbae14acae4b860a163cfe12b1fb46feba
Santiago Viana <68198881+SantiagodeViana@users.noreply.github.com>
2023-02-10 10:18:07 +0100

    Update Project Euler #020.py
    Comentarios

--- Python/Project Euler #020.py
+++ Python/Project Euler #020.py
@@ -1,7 +1,11 @@
+# Solución para el problema #20 de Project Euler
+# Halla la suma de los dígitos del número 100!
+# https://projecteuler.net/problem=20
+
 def sum_digits(n):
-    s = 0
+    s = 0 #Variable suma
     while n:
-        s += n % 10
+        s += n % 10 #Se suman los digitos, uno por uno
         n //= 10
     return s
 
