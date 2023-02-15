b16aca3d4042afd8d7bb3bb62a62cf6970baba4d
Santiago Viana <68198881+SantiagodeViana@users.noreply.github.com>
2023-02-15 10:44:45 +0100

    Update Main.java
    Comentario descripción

--- Java/ProjectEuler026/Main.java
+++ Java/ProjectEuler026/Main.java
@@ -1,6 +1,10 @@
 import java.util.HashMap;
 import java.util.Map;
 
+/* Solución para el problema #26 de Project Euler
+ * "Encuentra el valor de d < 1000 para el que 1/d contiene el ciclo recurrente más largo en su parte de fracción decimal"
+ * https://projecteuler.net/problem=26 */
+
 public class Main {
     public static void main(String[] args) {
         int respuesta = 0;
