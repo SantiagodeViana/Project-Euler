50abafb8cd5dc2ae65b4720db32cf21d7269acfb
Santiago Viana <68198881+SantiagodeViana@users.noreply.github.com>
2023-02-15 10:54:40 +0100

    Update Main.java
    Comentario descripción

--- Java/ProjectEuler076_(No_terminado)/Main.java
+++ Java/ProjectEuler076_(No_terminado)/Main.java
@@ -1,3 +1,7 @@
+/* Solución para el problema #76 de Project Euler
+ * "¿De cuántas formas distintas se pueden escribir cien como suma de al menos dos números enteros positivos?"
+ * https://projecteuler.net/problem=76 */ 
+
 public class Main {
     public static void main(String[] args) {
         int n = 5;
