51d6f4bf8acaae6e2b068ebd85318200296609d8
Santiago Viana <68198881+SantiagodeViana@users.noreply.github.com>
2023-02-23 12:14:06 +0100

    Update isPalindrome.c
    Renombre función

--- C/Funciones/isPalindrome.c
+++ C/Funciones/isPalindrome.c
@@ -1,7 +1,7 @@
 //Comprueba si un entero es palíndromo
 //Función pendiente por ser optimizada
 
-int palindrome(long long int n){ //Long long int debería presentar problemas cuando es mayor a 2^18
+int isPalindrome(long long int n){ //Long long int debería presentar problemas cuando es mayor a 2^18
     int i = 1, j, length = 0, digitos[20], temp = n; //i es el multiplicador de 10
     if (n < 10) return 1; //Acepta número de un dígito como palídromos; cambiar a conveniencia
     while (i <= n){
