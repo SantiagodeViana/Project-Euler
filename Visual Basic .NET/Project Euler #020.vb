dbf5d2b5b5bd61d8354448162f78e6de8e0763e5
Santiago Viana <68198881+SantiagodeViana@users.noreply.github.com>
2023-02-27 13:41:17 +0100

    Create Project Euler #020.vb

---
+++ Visual Basic .NET/Project Euler #020.vb
@@ -0,0 +1,35 @@
+Module Module1
+
+    Sub Main()
+        Dim n As Integer = 1
+        Dim n1 As Integer = 0
+        Dim respuesta As Long = 1
+
+        'Find the sum of the digits in the number 100!
+
+        Do Until n = 100
+
+            'Overflow cuando n = 95 o n = 14, dependiendo de a partir de donde se multiplique
+
+            'n1 = n
+            'n += 1
+            'respuesta = respuesta * n * n1 '.ToString?
+            'Console.Write(n)
+            'Console.Write("   ")
+            'Console.Write(n1)
+
+            respuesta = (2 * 3) * (4 * 5) * 6 * 7 * 8 * 9 * 11 * 12 * 13
+
+            'Se pueden remover los ceros de las decenas; por ejemplo, 10, 20, 30, multiplicando por 1, 2 y 3 respectivamente
+
+            Console.Write("   ")
+            Console.WriteLine(respuesta)
+
+        Loop
+
+        Console.WriteLine(respuesta)
+        Console.ReadLine()
+
+    End Sub
+    
+ End Module
