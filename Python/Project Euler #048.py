57e08d43e61a343e4d3624cfbb68bf82e8240dda
Santiago Viana <68198881+SantiagodeViana@users.noreply.github.com>
2023-02-28 11:29:30 +0100

    Update Project Euler #048.py
    Comentario incluyendo respuesta al problema. Arreglos menores

--- Python/Project Euler #048.py
+++ Python/Project Euler #048.py
@@ -4,8 +4,8 @@
 
 base = 1
 suma = 0
-while base <= 10: #Calcula la serie
+while base < 1000: #Calcula la serie
 	suma += (base ** base)
 	base += 1
-    	print base
-print suma
+print ("La respuesta es: ") #La respuesta es 9110846700
+print (suma % 10000000000) #Se agarran los diez últimos digitos de la potencia
