3817363d81f809701ecb3b575390942dcdbf36ff
Santiago Viana <68198881+SantiagodeViana@users.noreply.github.com>
2023-02-28 10:38:43 +0100

    Update Project Euler #025.py
    Comentario con respuesta al problema

--- Python/Project Euler #025.py
+++ Python/Project Euler #025.py
@@ -12,11 +12,14 @@ n1 = 0
 n2 = 0
 x = 0
 respuesta = 0
-while num_digits(x) < 1000: #Secuencia Fibonacci
-   	x = n1 + n2
+
+while num_digits(x) < 1000: #Se busca en la secuencia 
+   	x = n1 + n2 #Secuencia Fibonacci
    	if x == 0:
    		x += 1
    	respuesta += 1
-    	n2 = n1
+   	n2 = n1
    	n1 = x
-print respuesta
+
+print ("La respuesta es: ") #La respuesta es 4782
+print (respuesta)
