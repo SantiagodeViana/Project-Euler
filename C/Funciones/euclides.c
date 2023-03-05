e53f0f5b148b859df3b075c60808afd80a8f3031
Santiago Viana <68198881+SantiagodeViana@users.noreply.github.com>
2023-03-05 11:28:14 +0100

    Update and rename Euclides.c to euclides.c

--- C/Funciones/euclides.c
+++ C/Funciones/euclides.c
@@ -1,4 +1,4 @@
-int Euclides (int n, int m){ //Regresa MCD de a y b, donde 0 < b <= a
+int euclides (int n, int m){ //Regresa MCD de a y b, donde 0 < b <= a
     int a = n, b = m; //De manera predeterminada se asume que n es mayor que m
     if (a == b) return a; //Si a = b, son iguales al MCD
     if ((a%2 == 0) && (b%2 ==0)) return 2; //Ad hoc: Ambos números pares, es reducible
