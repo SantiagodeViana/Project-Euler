5c7a3a4a2f7a4d2f1e2d296e18483242c3106d5c
Santiago Viana <68198881+SantiagodeViana@users.noreply.github.com>
2023-03-26 23:18:19 +0100

    Create Project Euler #087.c

---
+++ C/Project Euler #087.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+int isPrime(int n);
+
+int main()
+{
+    int x, y, z, n, respuesta = 0;
+
+    for (n=1; n<=50000000 ; n++){ //50000000
+        for (x=2; (x*x*x*x) <= n ; x++){
+            if (isPrime(x) == 1){
+                for (y=2; (y*y*y) <= n ; y++){
+                    if (isPrime(y) == 1){
+                        for (z=2; (z*z) <= n ; z++){
+                            if (isPrime(z) == 1){
+                                if (((x*x*x*x) + (y*y*y) + (z*z)) == n){
+                                    respuesta++;
+                                    printf("n:%d, Respuesta:%d (x:%d y:%d z:%d)\n", n, respuesta, x, y, z);
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        if (n % 1000000 == 0) printf("n:%d, Respuesta:%d\n", n, respuesta);
+    }
+    printf("\nLa respuesta es:%d\n", respuesta);
+    printf("Fin del programa\n");
+    return 0;
+}
+
+int isPrime(int n){ //Función para determinar si un numero es primo.
+    if (n == 1) return 1; ////*Cambiar a 0 o mantener a conveniencia del programa
+    else if (n < 4) return 1; //Si son menores de 4, son primos
+    else if (n % 2 == 0) return 0; //Si son pares, no son primos
+    else if (n < 9) return 1; //Si son menores de 9 e impares, son primos
+    else if (n % 3 == 0) return 0; //Si son múltiplos de 3, no son primos
+    else{ //Criva de Erastóstenes
+        int root = floor(sqrt(n));
+        int i = 5;
+        while (i <= root){
+            if (n % i == 0) return 0;
+            else if (n % (i + 2) == 0) return 0;
+            else i += 6; //11, 17
+        }
+        return 1;
+    }
+}
