f93cfebcae1b5ee5554cc13a235dc7c43a2d5991
Santiago Viana <68198881+SantiagodeViana@users.noreply.github.com>
2023-04-02 12:25:46 +0100

    Create Project Euler #206.c

---
+++ C/Project Euler #206.c
@@ -0,0 +1,17 @@
+//Probar con Python
+
+#include <stdio.h>
+#include <stdlib.h>
+
+//Entre 10^10 y 1414213562
+
+int main()
+{
+    long long int i;
+    printf("%lld\n", 10000000000*1000000000);
+    //for (i = 10000000000; i <= 1414213562; i += 10){
+
+        //printf("%lld\n", i*i);
+    //}
+    return 0;
+}
