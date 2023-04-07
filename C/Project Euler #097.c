422a674aa98d4dcbed3e6638fa2ffd8a9b2fb591
Santiago Viana <68198881+SantiagodeViana@users.noreply.github.com>
2023-04-07 22:54:11 +0100

    Create Project Euler #097.c

---
+++ C/Project Euler #097.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+////EULER 97
+//Sólo probando los límites de long long
+//Jamás, ni remotamente, se puede resolver el problema por fuerza bruta con estas condiciones
+
+int main()
+{
+    long long i;
+    while(1 != 0){
+        i += 10000000000000;
+        if (i % 10000000000000 == 0) printf("%llu\n", i);
+    }
+    return 0;
+}
