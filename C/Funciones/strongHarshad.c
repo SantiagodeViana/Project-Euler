bf82e8c35a499508879ae83db5c607a35d3e3007
Santiago Viana <68198881+SantiagodeViana@users.noreply.github.com>
2023-04-11 18:18:11 +0200

    Create strongHarshad.c

---
+++ C/Funciones/strongHarshad.c
@@ -0,0 +1,8 @@
+//Función usada en Project Euler #387
+
+int strongHarshad(long long int n){
+    if (n % digitSum(n) != 0) return 0;
+    else if (n < 10) return 0;
+    else if (isPrime(n/digitSum(n)) == 1) return 1; //Si l división de la suma de sus dígitos es un número primo
+    return 0;
+}
