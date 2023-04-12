28108e2d57c4866f4545ab85705ae1cfb9f5617e
Santiago Viana <68198881+SantiagodeViana@users.noreply.github.com>
2023-04-12 09:31:43 +0200

    Create rightTruncatableHarshad.c

---
+++ C/Funciones/rightTruncatableHarshad.c
@@ -0,0 +1,14 @@
+//Función usada en Project Euler #387
+
+int rightTruncatableHarshad(long long int n){ //Desde la derecha
+    int i = 1, j;
+    if (n < 10) return 0;
+    else{
+        while (i < n){
+            j = n / i;
+            if (isHarshad(j) == 0) return 0; //Mientras recursivamente siempre siga dando números Harshad
+            i *= 10;
+        }
+    }
+    return 1;
+}
