949836ac221bcc3555f594cf38e086f7b2624ed7
Santiago Viana <68198881+SantiagodeViana@users.noreply.github.com>
2023-04-23 11:01:28 +0100

    Create Project Euler #034.vb

---
+++ Visual Basic .NET/Project Euler #034.vb
@@ -0,0 +1,35 @@
+Module Module1
+
+    Function factorial(ByVal number As Long) As Long
+        If number <= 1 Then
+            Return (1)
+        Else
+            Return number * factorial(number - 1)
+        End If
+    End Function
+
+    Sub Main()
+
+        Dim n As Integer = 11
+        Dim respuesta As Long = Nothing
+        Do Until respuesta = 40730
+            n += 1
+            Dim sum As Long = Nothing
+            Dim number As String = n.ToString 'String
+            Dim digit() As Char = number.ToCharArray() 'Dígitos
+            Dim n2 As Integer = Nothing
+            Do Until n2 >= digit.Length
+                Dim f As Integer = Integer.Parse(digit(n2)) 'Factorial del dígito
+                sum += factorial(f) 'Suma de los factoriales
+                n2 += 1
+            Loop
+            If sum = n Then 'Si la suma es igual al número, añade a respuesta
+                respuesta += n
+            End If
+            Console.WriteLine(n & "   " & sum & "   " & respuesta)
+        Loop
+        Console.WriteLine("La respuesta es " & respuesta) 'La respuesta es 40730
+        Console.ReadLine()
+    End Sub
+
+End Module
