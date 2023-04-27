54c85099220867d56ea0c311c51eea2c686b086c
Santiago Viana <68198881+SantiagodeViana@users.noreply.github.com>
2023-04-27 23:30:16 +0100

    Create isDeficient.vb

---
+++ Visual Basic .NET/Funciones/isDeficient.vb
@@ -0,0 +1,24 @@
+'Función usada en Project Euler #023
+
+Function isDeficient(ByVal n As Integer) As Boolean 'Función para determinar si el número es deficiente
+        If n < 12 And n <> 6 Then
+            Return True
+        ElseIf isPrime(n) Then
+            Return True
+        Else
+            Dim divisor As Integer = Nothing
+            Dim total As Integer = Nothing
+            Do While divisor <= (n / 2)
+                divisor += 1
+                If n Mod divisor = 0 Then
+                    total += divisor
+                End If
+            Loop
+            If total < n Then
+                Return True
+            ElseIf total >= n Then
+                Return False
+            End If
+            Return True
+        End If
+End Function
