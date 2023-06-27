6709ebb53b7d2f558ca6be1fc50ca8d44633fa27
Santiago Viana <68198881+SantiagodeViana@users.noreply.github.com>
2023-06-27 08:24:45 +0100

    Create Project Euler #053.vb

---
+++ Visual Basic .NET/Project Euler #053.vb
@@ -0,0 +1,27 @@
+Module Module1
+    Function factorial(ByVal n As Integer) As Long 'Función para un factorial (x!)
+        If n <= 1 Then
+            Return 1
+        Else
+            Return factorial(n - 1) * n
+        End If
+    End Function
+    Sub Main()
+        Dim n As Integer = Nothing
+        Dim r As Integer = Nothing
+        Dim c As Integer = Nothing
+        Do Until c > 1000000
+            If r < n Then
+                r += 1
+            ElseIf r >= n Then
+                n += 1
+                r = 2
+            End If
+            c = (factorial(n) / (factorial(r) * factorial(n - r)))
+            Console.WriteLine(n & "   " & r & "   " & c)
+            'Console.ReadLine()
+        Loop
+        Console.WriteLine("La respuesta es " & n) 'Overflow when n = 21 (21! > 1E+18)
+        Console.ReadLine()
+    End Sub
+End Module
