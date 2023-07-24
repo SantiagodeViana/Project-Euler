6648b8e25fd1125295109d567ee9a738663bc6bf
Santiago Viana <68198881+SantiagodeViana@users.noreply.github.com>
2023-07-24 11:16:29 +0100

    Create Project Euler #082 (incompleto).vb

---
+++ Visual Basic .NET/Project Euler #082 (incompleto).vb
@@ -0,0 +1,28 @@
+Module Module1
+
+    Sub Main()
+
+        Dim matrix = New Integer(79, 79)
+        Dim n As Integer = Nothing
+        Dim x As Integer = Nothing
+        Dim y As Integer = Nothing
+        Dim start As Integer = Nothing
+        Dim limit As Integer = 79
+
+        'Moviéndose arriba, a la derecha y abajo, consigue el camino en la matriz con la menor suma
+        Do Until x = 79 And y = 79
+
+            'Primer paso: Encontrar el número en la primera columna cuya suma con su número adyacente sea la menor
+            start += 1
+            n += 1
+
+            If matrix(start, x) + matrix(start, x + 1) Then
+
+            End If
+        Loop
+
+        'Segundo paso: Euler81
+
+    End Sub
+
+End Module
