bc842a777757ceb9c81374d8cdd66681bd21b233
Santiago Viana <68198881+SantiagodeViana@users.noreply.github.com>
2023-12-13 00:39:13 +0100

    Create Project Euler #075 (por revisar).py

---
+++ Python/Project Euler #075 (por revisar).py
@@ -0,0 +1,13 @@
+respuesta = 0
+
+for x in range(1, 1500001):
+    for y in range(x + 1, 1500001):
+        for z in range(y + 1, 1500001):
+            xC, yC, zC = x * x, y * y, z * z
+            if xC + yC == zC:
+                respuesta += 1
+                print(f"Respuesta: {respuesta} - {x+y+z} - x: {x} y: {y} z: {z}")
+            # print(f"x: {x} y: {y} z: {z}")
+
+print(f"La respuesta es: {respuesta}")
+print("Fin del programa.")
