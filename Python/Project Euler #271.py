5bf4dbbc4b3389b2b8019d71d1b40a5428c26e77
Santiago Viana <68198881+SantiagodeViana@users.noreply.github.com>
2025-09-12 22:24:57 +0200

    Update Project Euler #271.py
    Incluyendo URL y enunciado

--- Python/Project Euler #271.py
+++ Python/Project Euler #271.py
@@ -1,4 +1,11 @@
-#Project Euler 271
+# Project Euler 271:
+# 
+# "Para un número positivo n, definimos S(n) como la suma de los enteros x tales que 1 < x < n y x³ ≡ 1 (mod n).
+# Cuando n = 91, existen 8 posibles valores para x, a saber: 9, 16, 22, 29, 53, 74, 79, 81.
+# Así, S(91) = 9 + 16 + 22 + 29 + 53 + 74 + 79 + 81 = 363.
+# Encuentra S(13082761331670030).
+# 
+#https://projecteuler.net/problem=271
 
 def modCubo(n):
     suma = 0
