3b0b354b3522778ddf5ecd5853ff7533d4d6d23b
Santiago Viana <68198881+SantiagodeViana@users.noreply.github.com>
2025-09-12 08:42:27 +0200

    Update Project Euler #078 (por corregir).c

--- C/Project Euler #078 (por corregir).c
+++ C/Project Euler #078 (por corregir).c
@@ -2,17 +2,21 @@
 #include <stdlib.h>
 #include <math.h>
 
+#define N 500
+
 /* Solución para el problema #78 de Project Euler
  * https://projecteuler.net/problem=78
  */
 
-long long int partitions(int n, int k, long long int memo[][100]);
-long long int partitionsAux(int n);
+long long int partitions(int n, int k, long long int memo[][N]);
 
 int main()
 {
-    int n = 2, respuesta = -1;
-    long long int p, memo[100][100];
+    int n = 1, respuesta = -1;
+    long long int p, memo[N][N];
+    for (int i = 0; i <= N; i++){ //Inicializando lista
+        for (int j = 0; j <= N; j++) memo[i][j] = -1;
+    }
     while (respuesta == -1){
         p = partitions(n, n, memo);
         if (p % 1000000 == 0) respuesta = n;
@@ -23,7 +27,7 @@ int main()
     return 0;
 }
 
-long long int partitions(int n, int k, long long int memo[][100]){ //Devuelve el número de particiones de un n mediante un divide y vencerás. Diseñado para PE #76, #77 y #78
+long long int partitions(int n, int k, long long int memo[][N]){ //Devuelve el número de particiones de un n mediante un divide y vencerás. Diseñado para PE #76, #77 y #78
     if (n == 0) return 1;
     else if (n < 0 || k == 0) return 0;
     else if (memo[n][k] != -1) return memo[n][k];
@@ -31,11 +35,4 @@ long long int partitions(int n, int k, long long int memo[][100]){ //Devuelve el
         memo[n][k] = partitions(n, k-1, memo) + partitions(n-k, k, memo);
         return memo[n][k];
     }
-}
-long long int partitionsAux(int n) {
-    static long long memo[100][100];
-    for (int i = 0; i <= n; i++){
-        for (int j = 0; j <= n; j++) memo[i][j] = -1;
-    }
-    return partitions(n, n, memo);
 }
