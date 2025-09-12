dc7a275db9f1e7ecf6c61cd54217042157f3b0db
Santiago Viana <68198881+SantiagodeViana@users.noreply.github.com>
2025-09-12 22:18:34 +0200

    Update Project Euler #078 (por corregir).c
    Arreglando relleno de matriz. Problema de overflow a partir de n=407, convritiendo a Python

--- C/Project Euler #078 (por corregir).c
+++ C/Project Euler #078 (por corregir).c
@@ -6,22 +6,22 @@
 
 /* Solución para el problema #78 de Project Euler
  * https://projecteuler.net/problem=78
+ * Problema de overflow a partir de n = 407
  */
 
 long long int partitions(int n, int k, long long int memo[][N]);
 
-int main()
-{
+int main(){
     int n = 1, respuesta = -1;
     long long int p, memo[N][N];
-    for (int i = 0; i <= N; i++){ //Inicializando lista
-        for (int j = 0; j <= N; j++) memo[i][j] = -1;
+    for (int i = 0; i < N; i++){ //Inicializando lista
+        for (int j = 0; j < N; j++) memo[i][j] = -1;
     }
     while (respuesta == -1){
         p = partitions(n, n, memo);
         if (p % 1000000 == 0) respuesta = n;
         else n++;
-        printf("p(%d): %d, respuesta: %d \n", n, p, respuesta);
+        printf("p(%d): %lld, respuesta: %d \n", n, p, respuesta);
     }
     printf("La respuesta es %d \n", respuesta);
     return 0;
