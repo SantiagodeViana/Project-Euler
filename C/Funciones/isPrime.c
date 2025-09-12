f8eb96ba7953286cc4c72e87f05665eb28b26173
Santiago Viana <68198881+SantiagodeViana@users.noreply.github.com>
2025-09-12 20:35:42 +0200

    Update isPrime.c
    == 1 return 0 default

--- C/Funciones/isPrime.c
+++ C/Funciones/isPrime.c
@@ -1,5 +1,5 @@
 int isPrime(int n){//Función para determinar si un numero es primo, basada en la Criba de Eratóstenes
-    if (n == 1) return 1; ////*Cambiar a 0 o mantener a conveniencia del programa
+    if (n == 1) return 0; ////*Cambiar a 1 o mantener a conveniencia del programa
     else if (n < 4) return 1; //Si son menores de 4, son primos
     else if (n % 2 == 0) return 0; //Si son pares, no son primos
     else if (n < 9) return 1; //Si son menores de 9 e impares, son primos
@@ -15,3 +15,4 @@ int isPrime(int n){//Función para determinar si un numero es primo, basada en l
         return 1;
     }
 }
+
