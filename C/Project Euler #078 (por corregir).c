164e38e787740b1a5c62c8508c9ef2735d420cb2
Santiago Viana <68198881+SantiagodeViana@users.noreply.github.com>
2025-09-12 08:07:06 +0200

    Update Project Euler #078 (por corregir).c
    Actualizando a función p(n, k)

--- C/Project Euler #078 (por corregir).c
+++ C/Project Euler #078 (por corregir).c
@@ -6,23 +6,24 @@
  * https://projecteuler.net/problem=78
  */
 
-int partitions(int n);
+long long int partitions(int n, int k);
 
 int main()
 {
-    int n = 5, respuesta;
-    respuesta = partitions(n);
-    printf("La respuesta es %d", respuesta);
+    int n = 1, p, respuesta = -1;
+    while (respuesta == -1){
+        p = partitions(n, n);
+        if (p % 1000000 == 0) respuesta = n;
+        else n++;
+        printf("p(%d): %d, respuesta: %d \n", n, p, respuesta);
+    }
+    respuesta = partitions(n, n);
+    printf("La respuesta es %d \n", respuesta);
     return 0;
 }
 
-int partitions(int n){ //Devuelve el número de particiones de un n mediante un divide y vencerás. Diseñado para PE #76, #77 y #78
-//Queda pendiente considerar particiones >ceil
-  int x, y;
-  if (n != 1){
-    x = ceil(n/2);
-    y = floor(n/2);
-    return (partitions(x) + partitions(y));
-  }
-  else return 1;
+long long int partitions(int n, int k){ //Devuelve el número de particiones de un n mediante un divide y vencerás. Diseñado para PE #76, #77 y #78
+    if (n == 0) return 1;
+    else if (n < 0 || k == 0) return 0;
+    else return (partitions(n, k - 1) + partitions(n - k, k));
 }
