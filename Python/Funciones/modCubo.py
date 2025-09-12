29bdd74bfc89e08a5839017e83b9eb2e6ab3b015
Santiago Viana <68198881+SantiagodeViana@users.noreply.github.com>
2025-09-12 22:28:15 +0200

    Create modCubo.py
    Función #271

---
+++ Python/Funciones/modCubo.py
@@ -0,0 +1,13 @@
+# Función usada en Project Euler #271
+
+def modCubo(n):
+    suma = 0
+    for i in range(2, n, 1):  # Sola cuenta números impares
+        aux = i
+        for j in range(2):  # x^2 mod n
+            aux %= n
+            aux *= i
+        if aux % n == 1:
+            suma += i  # suma += x
+            print("x:", i)
+    return suma
