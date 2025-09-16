6bfc2a58e0ec6e6768226c3a7fa77ac545c26798
Santiago Viana <68198881+SantiagodeViana@users.noreply.github.com>
2025-09-16 20:29:18 +0200

    Update Project Euler #095 (por corregir).c
    Reescribiendo código. Probando función isPerfect.

--- C/Project Euler #095 (por corregir).c
+++ C/Project Euler #095 (por corregir).c
@@ -4,53 +4,25 @@
 /* Solución para el problema #95 de Project Euler
  * https://projecteuler.net/problem=95            */
 
-int suma_divisores(int n) {
-    int suma = 0;
-    for (int divisor = 1; divisor <= n / 2; divisor++) {
-        if (n % divisor == 0) suma += divisor;
-    }
-    return suma;
-}
+int isPerfect(int n);
 
 int main() {
-    int suma = 0, suma2 = 0, divisor = 1, dividendo = 1, amicable1 = 0, amicable2 = 0;
-    long long int respuesta = 0;
-
-    while (dividendo <= 10000) { // Se buscan y suman los divisores de un número
-        if (dividendo % divisor == 0) suma += divisor;
-        divisor++;
-        if (divisor > (dividendo / 2) && dividendo != suma && suma > 0) {  // Al terminar de buscar los divisores...
-            amicable1 = dividendo;  // ...se buscarán números amicables
-            amicable2 = suma;
-            divisor = 1;
-            dividendo++;
-            suma = 0;
-            suma2 = 0;
-            while (divisor <= (amicable2 / 2)) { // Se buscan y suman los divisores de la suma de divisores
-                if (amicable2 % divisor == 0) suma2 += divisor;
-                divisor++;
-            }
-        }
-        else if (divisor > (dividendo / 2)) {
-            suma = 0;
-            divisor = 1;
-            dividendo++;
-        }
-
-        if (amicable1 == suma2) { // Si el número es amicable
-            respuesta += amicable1;
-            respuesta += amicable2;
-            printf("Amicable1: %d   Amicable2: %d   Respuesta: %ld   Suma2: %d\n", amicable1, amicable2, respuesta, suma2);
-            suma2 = 0;
-            divisor = 1;
-        }
-
-        if (divisor > dividendo) {
-            divisor = 1;
-        }
+    for (int i = 0; i <= 8589869056; i++){
+        if (isPerfect(i) == 1) printf("Numero perfecto: %d\n", i);
     }
-
-    respuesta = (respuesta - 11088) / 2 + 11088; // Ajustar el resultado para eliminar las repeticiones
-    printf("La respuesta es %ld\n", respuesta);
     return 0;
+}
+
+int isPerfect(int n){ //Función mencionada en Project Euler #95
+    //Un número perfecto es aquel n cuya suma de divisores es igual a n
+    int i = 1, perfect = 1, divisores = 0; //i es el contador del bucle, perfect un booleano, y divisores la suma de los divisores de n
+    if (n < 6) perfect = 0; //El primer número perfecto es 6
+    while (perfect == 1 || i <= (n/2)){ //Se buscan todos los divisores de n y se suman
+        if (n % i == 0){
+            divisores += i;
+            if (divisores > n) perfect = 0; //n no es un número perfecto si la suma de los divisores excede n
+        }
+        i++;
+    }
+    return perfect;
 }
