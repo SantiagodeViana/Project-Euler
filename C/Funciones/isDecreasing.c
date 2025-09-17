1bf962209da146bb85851322fdd963f0ace2ebf9
Santiago Viana <68198881+SantiagodeViana@users.noreply.github.com>
2025-09-17 15:34:56 +0200

    Update isDecreasing.c

--- C/Funciones/isDecreasing.c
+++ C/Funciones/isDecreasing.c
@@ -1,14 +1,11 @@
-//Función mencionada en Project Euler #112
+//Función mencionada en Project Euler #112 y función isBouncy
 
 int isDecreasing (int n){
-     int aux, dec = 1;
-    if (n > 9){
+    int aux = n % 10, dec = 1;
+    while (n >= 10 && dec == 1){
+        n /= 10;
+        if (n % 10 < aux) dec = 0;
         aux = n % 10;
-        while (n > 0 || dec == 1){
-            n /= 10;
-            if (n % 10 < aux) dec = 0;
-            aux = n % 10;
-        }
     }
     return dec;
  }
