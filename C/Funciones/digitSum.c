61526e45045fb82b8876ea7511da1881ae3a83eb
Santiago Viana <68198881+SantiagodeViana@users.noreply.github.com>
2025-09-18 00:21:21 +0200

    Update digitSum.c
    Función errónea; colocando while para no depender de otra función. También usada en Project Euler #119.

--- C/Funciones/digitSum.c
+++ C/Funciones/digitSum.c
@@ -1,8 +1,8 @@
-//Función usada en Project Euler #387
+//Función usada en Project Euler #119 y #387
 
 int digitSum(long long int n){ //Función para sumar dígitos de un entero
     int i, suma = 0;
-    for (i=0; i<=digitos(n)+1;i++){
+    while (n > 0){
         suma += n % 10;
         n/=10;
     }
