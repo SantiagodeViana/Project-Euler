42096b13ded042aee1cc125c67208915e847b6a1
Santiago Viana <68198881+SantiagodeViana@users.noreply.github.com>
2025-09-18 00:45:54 +0200

    Update digitSum.c
    -i;

--- C/Funciones/digitSum.c
+++ C/Funciones/digitSum.c
@@ -1,7 +1,7 @@
 //Función usada en Project Euler #119 y #387
 
 int digitSum(long long int n){ //Función para sumar dígitos de un entero
-    int i, suma = 0;
+    int suma = 0;
     while (n > 0){
         suma += n % 10;
         n/=10;
