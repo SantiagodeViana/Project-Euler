191bdfb640e32bbd7bd324a3d5868481c36b6fc7
Santiago Viana <68198881+SantiagodeViana@users.noreply.github.com>
2025-09-17 15:36:14 +0200

    Update isBouncy.c

--- C/Funciones/isBouncy.c
+++ C/Funciones/isBouncy.c
@@ -1,4 +1,10 @@
-#include <stdio.h>
-#include <stdlib.h>
+//Función usada en Project Euler #112
 
-int isBouncy(int n); //Función de Project Euler #112
+int isBouncy (int n){
+    int bounce = 1;
+    if (n < 100) bounce = 0; //No hay bouncy numbers menores que 100
+    else{
+        if (isDecreasing(n) == 1 || isIncreasing(n) == 1) bounce = 0; //Un bouncy number no es ni incremental ni decremental
+    }
+    return bounce; //La operación también puede hacerse con un solo chequeo, pero el número probablemente deba ser tratado como una cadena
+}
