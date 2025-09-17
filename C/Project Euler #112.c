939d5596afb272dd1188981011f9551a7a6f961b
Santiago Viana <68198881+SantiagodeViana@users.noreply.github.com>
2025-09-17 15:50:53 +0200

    Create Project Euler #112.c
    https://projecteuler.net/problem=112

---
+++ C/Project Euler #112.c
@@ -0,0 +1,55 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* Solución para el problema #112 de Project Euler
+ * Encuentra el menor número para el cual la proporción de números bouncy es exactamente 99%.
+ * https://projecteuler.net/problem=112 */
+
+int isIncreasing (int n);
+int isDecreasing (int n);
+int isBouncy (int n);
+
+int main()
+{
+    //printf("134468 increasing? %d\n", isIncreasing(134468));
+    //printf("66420 decreasing? %d\n", isDecreasing(66420));
+    //printf("155349 bouncy? %d\n", isBouncy(155349));
+    int i;
+    double bounces = 0, respuesta;
+    for (i = 1; respuesta < 0.99; i++){
+        if (isBouncy(i) == 1){
+            bounces++;
+            respuesta = bounces/i;
+            printf("i: %d, respuesta %f (%f)\n", i, respuesta, bounces);
+        }
+    }
+    printf("La respuesta es %d", i-1);
+    return 0;
+}
+
+int isIncreasing (int n){
+    int aux = n % 10, inc = 1;
+    while (n >= 10 && inc == 1){
+        n /= 10;
+        if (n % 10 > aux) inc = 0;
+        aux = n % 10;
+    }
+    return inc;
+ }
+int isDecreasing (int n){
+    int aux = n % 10, dec = 1;
+    while (n >= 10 && dec == 1){
+        n /= 10;
+        if (n % 10 < aux) dec = 0;
+        aux = n % 10;
+    }
+    return dec;
+ }
+int isBouncy (int n){
+    int bounce = 1;
+    if (n < 100) bounce = 0; //No hay bouncy numbers menores que 100
+    else{
+        if (isDecreasing(n) == 1 || isIncreasing(n) == 1) bounce = 0; //Un bouncy number no es ni incremental ni decremental
+    }
+    return bounce; //La operación también puede hacerse con un solo chequeo, pero el número probablemente deba ser tratado como una cadena
+}
