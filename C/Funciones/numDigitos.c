64d8238fa51a2206b7d301fb8393913d5952bc73
Santiago Viana <68198881+SantiagodeViana@users.noreply.github.com>
2025-09-18 00:11:25 +0200

    Update and rename digitos.c to numDigitos.c

--- C/Funciones/numDigitos.c
+++ C/Funciones/numDigitos.c
@@ -1,6 +1,6 @@
 //Función usada en Project Euler #59 y #387
 
-int digitos(int n){ //Cuenta los dígitos de una cifra
+int numDigitos(int n){ //Cuenta los dígitos de una cifra
     int respuesta = 0, i = 1;
     while (i <= n){
         i*=10;
