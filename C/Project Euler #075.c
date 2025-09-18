21b056a1caafec068cd0d80a6ce589018fa8867d
Santiago Viana <68198881+SantiagodeViana@users.noreply.github.com>
2025-09-19 00:03:26 +0200

    Update Project Euler #075.c
    "La inicialización es de la menor terna pitagórica"

--- C/Project Euler #075.c
+++ C/Project Euler #075.c
@@ -5,7 +5,7 @@
 
 int main()
 {
-    int x = 3, y = 4, z = 5, respuesta = 0;
+    int x = 3, y = 4, z = 5, respuesta = 0; //La inicialización es de la menor terna pitagórica
     long long int a, b, c;
     while (x < N){
         while (y < N && y < (x + z)){ //Aplicando desigualdad triangular: z no puede ser mayor o igual que la suma de los otros dos lados
