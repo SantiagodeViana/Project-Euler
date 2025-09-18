e60b2072d8520d62c63318c24565dfc63ac610b0
Santiago Viana <68198881+SantiagodeViana@users.noreply.github.com>
2025-09-18 02:02:28 +0200

    Create Project Euler #119.c

---
+++ C/Project Euler #119.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* Solución para el problema #119 de Project Euler
+ * https://projecteuler.net/problem=119 */
+
+int digitSum(long long int n);
+long long int digitPowerSum(long long int n);
+
+int main()
+{
+    long long int i;
+    int n = 19; //n va a ser el índice de los términos de la secuencia con la propiedad buscada
+    for (i = 23350000000; n < 30; i++){
+        if (digitPowerSum(i) == 1){
+            n++;
+            printf("%d) %lld is a digitPowerSum\n", n, i);
+        }
+        if (i % 10000000 == 0) printf("i: %lld n: %d\n", i, n);
+    }
+    printf("\nLa respuesta es %lld", i-1);
+    return 0;
+}
+
+int digitSum(long long int n){ //Función para sumar dígitos de un entero
+    int suma = 0;
+    while (n > 0){
+        suma += n % 10;
+        n/=10;
+    }
+    return suma;
+}
+
+long long int digitPowerSum(long long int n){
+    int powerSum = 0, power; //powerSum es un booleano
+    long long int suma;
+    power = digitSum(n); //Potencia
+    if (n % 2 != 0 && power % 2 == 0) return 0; //Criva para descartar factores
+    else if (n % 3 != 0 && power % 3 == 0) return 0;
+    else if (n % 5 != 0 && power % 5 == 0) return 0;
+    else if (n % 7 != 0 && power % 7 == 0) return 0;
+    else{
+        suma = power; //Resultado final
+        while (suma < n && power != 1){
+            if (suma > n / power) break;  //Evitando casos de desbordamiento
+            else suma *= power; //Se multiplica (eleva a la potencia) a la digitSum hasta determinar si es una potencia de n. Se ignoran los 1
+        }
+        if (suma == n) powerSum = 1;
+    }
+    return powerSum;
+}
