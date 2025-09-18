d89243d3e5c374dcd8c7ecfe846c3423febaa571
Santiago Viana <68198881+SantiagodeViana@users.noreply.github.com>
2025-09-18 03:53:45 +0200

    Update Project Euler #119.c
    Mejorando recortes

--- C/Project Euler #119.c
+++ C/Project Euler #119.c
@@ -10,13 +10,13 @@ long long int digitPowerSum(long long int n);
 int main()
 {
     long long int i;
-    int n = 19; //n va a ser el índice de los términos de la secuencia con la propiedad buscada
-    for (i = 23350000000; n < 30; i++){
+    int n = 23; //n va a ser el índice de los términos de la secuencia con la propiedad buscada
+    for (i = 78200000000; n < 30; i++){ //Último número probado: 78200000000
         if (digitPowerSum(i) == 1){
             n++;
             printf("%d) %lld is a digitPowerSum\n", n, i);
         }
-        if (i % 10000000 == 0) printf("i: %lld n: %d\n", i, n);
+        if (i % 100000000 == 0) printf("i: %lld n: %d\n", i, n);
     }
     printf("\nLa respuesta es %lld", i-1);
     return 0;
@@ -30,20 +30,20 @@ int digitSum(long long int n){ //Función para sumar dígitos de un entero
     }
     return suma;
 }
-
 long long int digitPowerSum(long long int n){
     int powerSum = 0, power; //powerSum es un booleano
     long long int suma;
     power = digitSum(n); //Potencia
-    if (n % 2 != 0 && power % 2 == 0) return 0; //Criva para descartar factores
-    else if (n % 3 != 0 && power % 3 == 0) return 0;
+    if (power == 1) return 0; //Se ignoran los 1
+    else if (n % 2 != 0 && power % 2 == 0) return 0; //Criva para descartar factores
+    else if (n % 3 != 0 && power % 9 == 0) return 0; //n % 3 siempre es igual a power % 3
     else if (n % 5 != 0 && power % 5 == 0) return 0;
     else if (n % 7 != 0 && power % 7 == 0) return 0;
     else{
         suma = power; //Resultado final
-        while (suma < n && power != 1){
+        while (suma < n){
             if (suma > n / power) break;  //Evitando casos de desbordamiento
-            else suma *= power; //Se multiplica (eleva a la potencia) a la digitSum hasta determinar si es una potencia de n. Se ignoran los 1
+            else suma *= power; //Se multiplica (eleva a la potencia) a la digitSum hasta determinar si es una potencia de n
         }
         if (suma == n) powerSum = 1;
     }
