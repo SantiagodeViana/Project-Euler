89d97dd90fad8613165a152b9f22dd7f0538ac20
Santiago Viana <68198881+SantiagodeViana@users.noreply.github.com>
2025-09-18 05:19:58 +0200

    Update Project Euler #119.c
    Buscando potencias primero y después la suma de dígitos, en vez de calcular todas las sumas de dígitos y después las potencias.

--- C/Project Euler #119.c
+++ C/Project Euler #119.c
@@ -2,23 +2,37 @@
 #include <stdlib.h>
 
 /* Solución para el problema #119 de Project Euler
+ * El programa arroja los diferentes términos y candidatos desordenados y repetidos. Deben ordenarse manualmente.
  * https://projecteuler.net/problem=119 */
 
 int digitSum(long long int n);
-int digitPowerSum(long long int n);
 
 int main()
 {
-    long long int i;
-    int n = 23; //n va a ser el índice de los términos de la secuencia con la propiedad buscada
-    for (i = 78200000000; n < 30; i++){ //Último número probado: 78200000000
-        if (digitPowerSum(i) == 1){
-            n++;
-            printf("%d) %lld is a digitPowerSum\n", n, i);
+    int i, j, k, n = 0, suma; //n va a ser el índice de los términos de la secuencia con la propiedad buscada
+    long long int aux, power;
+    for (i = 2; n < 50; i++){ //Primer bucle: base
+        for (j = 1; j < 63; j++){ //Segundo bucle: exponente. 2^63 es la última potencia antes que > LLONG_MAX
+            power = i;
+            k = 1;
+            while ((power <= LLONG_MAX / i) && k < j){ //Tercer bucle: multiplicación para evitar pow y doubles
+                 power *= i;
+                 k++;
+            }
+            suma = digitSum(power);
+            if (power % suma == 0 && suma != 1){
+                aux = power;
+                while (aux % suma == 0){
+                    aux /= suma; //Se divide constantemente la potencia de manera que los únicos divisores sean el digitSum
+                    if (aux == suma){ //Si se llega al final del mcm, se ha conseguido la digitPowerSum
+                        printf("%lld\n", power);
+                        n++; //Se suma la cantidad de digitPowerSums
+                    }
+                }
+            }
         }
-        if (i % 100000000 == 0) printf("i: %lld n: %d\n", i, n);
+
     }
-    printf("\nLa respuesta es %lld", i-1);
     return 0;
 }
 
@@ -29,23 +43,4 @@ int digitSum(long long int n){ //Función para sumar dígitos de un entero
         n/=10;
     }
     return suma;
-}
-int digitPowerSum(long long int n){
-    int powerSum = 0, power; //powerSum es un booleano
-    long long int suma;
-    power = digitSum(n); //Potencia
-    if (power == 1) return 0; //Se ignoran los 1
-    else if (n % 2 != 0 && power % 2 == 0) return 0; //Criva para descartar factores
-    else if (n % 3 != 0 && power % 9 == 0) return 0; //n % 3 siempre es igual a power % 3
-    else if (n % 5 != 0 && power % 5 == 0) return 0;
-    else if (n % 7 != 0 && power % 7 == 0) return 0;
-    else{
-        suma = power; //Resultado final
-        while (suma < n){
-            if (suma > n / power) break;  //Evitando casos de desbordamiento
-            else suma *= power; //Se multiplica (eleva a la potencia) a la digitSum hasta determinar si es una potencia de n
-        }
-        if (suma == n) powerSum = 1;
-    }
-    return powerSum;
 }
