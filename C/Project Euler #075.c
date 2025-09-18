7bf1a69aa9a753a3b53a994025ad6832f8354eb3
Santiago Viana <68198881+SantiagodeViana@users.noreply.github.com>
2025-09-19 00:01:46 +0200

    Update and rename Project Euler #075 (overflow).c to Project Euler #075.c
    Se reemplazan los bucles for por while y se arreglan las condiciones.

---
+++ C/Project Euler #075.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define N 1500000
+
+int main()
+{
+    int x = 3, y = 4, z = 5, respuesta = 0;
+    long long int a, b, c;
+    while (x < N){
+        while (y < N && y < (x + z)){ //Aplicando desigualdad triangular: z no puede ser mayor o igual que la suma de los otros dos lados
+            while (z < N && z < (x + y)){
+                if ((x + y + z) == N){ //Sólo se consideran lados que resulten en el N objetivo
+                    a = x*x; b = y*y; c = z*z; //Inicializando cuadrados
+                    if (a + b == c){ //Teorema de Pitágoras (cat^2 + cat^2 = hip^2)
+                        respuesta++;
+                        printf("Respuesta:%d - %d - x:%d y:%d z:%d\n", respuesta, (x + y + z), x, y, z);
+                    }
+                }
+                z++;
+            }
+            y++;
+            z=5;
+        }
+        if (x < (y + z)) x++;
+        y=4;
+        printf("x:%d y:%d z:%d\n", x, y, z);
+    }
+    printf("La respuesta es: %d\n", respuesta);
+    printf("Fin del programa.\n");
+    return 0;
+}
