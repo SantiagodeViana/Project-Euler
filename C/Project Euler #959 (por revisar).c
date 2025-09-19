222d8105d13346062001ff54990d2ce691fe17b8
Santiago Viana <68198881+SantiagodeViana@users.noreply.github.com>
2025-09-19 03:00:29 +0200

    Update Project Euler #959 (por revisar).c
    Incluyendo array para registrar posiciones. Ocupa demasiada memoria, hace falta un array dinámico.

--- C/Project Euler #959 (por revisar).c
+++ C/Project Euler #959 (por revisar).c
@@ -2,30 +2,33 @@
 #include <stdlib.h> // srand(), rand()
 #include <time.h>  // time()
 
+#define N 1000000
+
 int coinFlip();
 
 int main()
 {
-    int a = 1, b = 2, aux = 0, posicion = 0, min = 0, max = 0, coin; //a son saltos hacia la izq, b hacia la derecha
-    double respuesta = 0; //Aux registra la cantidad de pasos (números) únicos explorados en ese paso. Respuesta calcula el promedio.
+    int a = 1, b = 2, aux = 0, posicion = N/2, coin; //a son saltos hacia la izq, b hacia la derecha. Aux registra la cantidad de pasos (números) únicos explorados.
+    int numberLine[N]; //Representación de la "number line"
+    double respuesta = 0; // Respuesta calcula el promedio.
     long long int n = 0; //n es la cantidad de pasos totales
+
     srand(time(NULL));
+
+    for (int i = 0; i < N; i++) numberLine[i] = -1; //Inicializando matriz de posiciones
     do{
         coin = coinFlip(); //Se decide la posición a la cual moverse
         if (coin == 0) posicion -= a;
         else if (coin == 1) posicion += b;
-        if (posicion > max){
-            aux += abs(posicion - max);
-            max = posicion;
-        }
-        else if (posicion < min){
-            aux += abs(posicion - min);
-            min = posicion;
+        if (numberLine[posicion] == -1){ //Se marca y se cuentan las posiciones visitadas
+            numberLine[posicion] = 1;
+            aux = 1;
         }
         respuesta = ((respuesta * n) + aux) / (n + 1);
-        aux = 0; //Reiniciando aux
         n++;
-        if (n % 5000000 == 0) printf("Pasos nuevos encontrados: %.12f\n", respuesta);
+        aux = 0; //Reiniciando aux
+        printf("Respuesta: %.12f\n", respuesta); //if (n % 5000000 == 0)
+        //getchar();
     } while (1);
 
     return 0;
