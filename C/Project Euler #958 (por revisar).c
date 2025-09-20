c524e51e575b97f8ff12ae80692be16c84bf98af
Santiago Viana <68198881+SantiagodeViana@users.noreply.github.com>
2025-09-21 00:16:34 +0200

    Update Project Euler #958 (por revisar).c
    Retocando pasosEuclides(), quitando operaciones modulares por restas

--- C/Project Euler #958 (por revisar).c
+++ C/Project Euler #958 (por revisar).c
@@ -19,23 +19,12 @@ int main()
 }
 
 int pasosEuclides (long long int n, long long int m){ //Algoritmo de Euclides modificado para devolver número de pasos requeridos para obtener MCD
-    int pasos = 0;
-    long long int a = n, b = m; //De manera predeterminada se asume que n es mayor que m
-    if (a == b) return a; //Si a = b, son iguales al MCD
-    else if (n < m){ //Si n es menor que m,
-        a = m;
-        b = n;
-    }
-    while ((a != 0) && (b != 0) && (a != 1) && (b != 1) && (a % b != 0)){ //Los restos se van turnando, de mayor a menor;
-        if ((b != 0) && (a != 1) && (b != 1)){
-            a = (a % b);
-            pasos++;
-        }
-        if ((a != 0) && (a != 1) && (b != 1)){
-            b = (b % a);
-            pasos++;
-        }
+    long long int a = n, b = m, pasos = 1; //De manera predeterminada se asume que n es mayor que m
+    while (a != b){ //Los restos se van turnando, de mayor a menor;
+        if (a > b) a -= b ;
+        else if (a < b) b -= a;
+        pasos ++;
     }
     return pasos;
-    return 1;
 }
+
