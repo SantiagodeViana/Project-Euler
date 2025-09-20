581e0b04d6d59a5abbf74ced524c6bca36561c2e
Santiago Viana <68198881+SantiagodeViana@users.noreply.github.com>
2025-09-21 00:29:38 +0200

    Update Project Euler #958 (por revisar).c
    Agregando pow al algoritmo de substracción para reducir operaciones mientras se mantienen pasos

--- C/Project Euler #958 (por revisar).c
+++ C/Project Euler #958 (por revisar).c
@@ -19,12 +19,23 @@ int main()
 }
 
 int pasosEuclides (long long int n, long long int m){ //Algoritmo de Euclides modificado para devolver número de pasos requeridos para obtener MCD
-    long long int a = n, b = m, pasos = 1; //De manera predeterminada se asume que n es mayor que m
-    while (a != b){ //Los restos se van turnando, de mayor a menor;
-        if (a > b) a -= b ;
-        else if (a < b) b -= a;
-        pasos ++;
+    int bound = floor(sqrt(n)), pow = 1; //Pow multiplica a o b y la cantidad de pasos en potencias de diez para ahorrar operaciones
+    long long int a = n, b = m, pasos = 0; //De manera predeterminada se asume que n es mayor que m
+    if (a == b) return a; //Si a = b, son iguales al MCD
+    while (a != b && pasos < bound){ //Los restos se van turnando, de mayor a menor;
+        if (a > b){
+            while (a > b*pow) pow *= 10;
+            while (a < b*pow) pow /= 10;
+            a -= b * pow;
+        } else if (a < b){
+            while (b > a*pow) pow *= 10;
+            while (b < a*pow) pow /= 10;
+            b -= a;
+        }
+        pasos += 1 * pow;
+        // printf("a: %lld b: %lld Pasos:%lld\n", a, b, pasos);
     }
     return pasos;
 }
+
 
