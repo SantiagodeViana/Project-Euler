81e5683b3b66925b0c3c0afc19d944fdb1a9eca8
Santiago Viana <68198881+SantiagodeViana@users.noreply.github.com>
2025-09-21 00:48:22 +0200

    Update euclides.c

--- C/Funciones/euclides.c
+++ C/Funciones/euclides.c
@@ -9,8 +9,8 @@ int euclides (int n, int m){ //Regresa MCD de a y b, donde 0 < b <= a
         b = n;
     }
     while ((a != 0) && (b != 0) && (a != 1) && (b != 1) && (a % b != 0)){ //Los restos se van turnando, de mayor a menor; // && (b % a != 0 &&
-        if ((b != 0) && (a != 1) && (b != 1)) a = (a % b);
-        if ((a != 0) && (a != 1) && (b != 1)) b = (b % a);
+        if ((b != 0) && (a != 1) && (b != 1)) a %= b;
+        if ((a != 0) && (a != 1) && (b != 1)) b %= a;
     }
     if (b == 0) return a; //En el caso de los pares, devolver el resto diferente de 0
     else if (a == 0) return b; //En el caso de los pares, devolver el resto diferente de 0
