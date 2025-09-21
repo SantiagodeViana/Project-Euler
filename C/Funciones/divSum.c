e0b5bfb9bfff6a479f3b36313e8812a642385ee4
Santiago Viana <68198881+SantiagodeViana@users.noreply.github.com>
2025-09-21 11:17:11 +0200

    Create divSum.c

---
+++ C/Funciones/divSum.c
@@ -0,0 +1,10 @@
+//Función usada en Project Euler #95
+
+int divSum(int n){ //Función para sumar divisores de n
+    int i = 1, div = 0; //Índice y suma de divisores
+    while (i <= (n/2)){
+        if (n % i == 0) div += i;
+        i++;
+    }
+    return div;
+}
