d979e4bb2b9c32c19cf6f789a635368f3ae2d1cb
Santiago Viana <68198881+SantiagodeViana@users.noreply.github.com>
2025-09-23 03:27:06 +0200

    Create Project Euler #080 (por corregir).c

---
+++ C/Project Euler #080 (por corregir).c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+/* Solución para el problema #80 de Project Euler
+ * https://projecteuler.net/problem=80            */
+
+int main()
+{
+    int respuesta = 0;
+    long double aux = sqrt(2);
+    for (int i = 1; i <= 100; i++){
+        aux *= 10; //Se quita la parte binaria y se almacena el siguiente decimal
+        respuesta += aux;
+        printf("%d) Respuesta: %d\n", i, respuesta);
+        aux /= 10;
+    }
+    printf("La respuesta es %d", respuesta);
+    return 0;
+}
