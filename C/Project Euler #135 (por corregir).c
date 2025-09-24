3ea09bc503a49c1092d0f0230155c1a859627ca1
Santiago Viana <68198881+SantiagodeViana@users.noreply.github.com>
2025-09-24 07:30:10 +0200

    Create Project Euler #135 (por corregir).c
    Solución para el problema #135 de Project Euler

---
+++ C/Project Euler #135 (por corregir).c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+ /* Solución para el problema #135 de Project Euler
+  * https://projecteuler.net/problem=135 */
+
+int main()
+{
+    int x, y, z, d = 1, soluciones = 0; //x es el mayor cuadrado, d la diferencia
+    for (int n = 1; n <= 100; n++){
+        x = n*2;
+        while (x > 2){ //3 es el valor mínimo antes de que z = 0 con la mínima diferencia
+            y = x - d;
+            z = x - 2*d;
+            if (((x*x) - (y*y) - (z*z)) > n) d++; //Se aumenta la diferencia en caso de que la expresión sea mayor
+            else if (((x*x) - (y*y) - (z*z)) <= n){
+                if (((x*x) - (y*y) - (z*z)) == n){
+                    soluciones++;
+                    printf("%d^2 - %d^2 - %d^2 = %d\n", x, y, z, n);
+                }
+                x--;
+                d = 1;
+            }
+        }
+        soluciones = 0;
+    }
+    printf("Hello world!\n");
+    return 0;
+}
