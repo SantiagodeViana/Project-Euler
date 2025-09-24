fa2bef036d91ef33d6b2ed3fec55d968ccd5598b
Santiago Viana <68198881+SantiagodeViana@users.noreply.github.com>
2025-09-24 21:08:17 +0200

    Create Project Euler #173 (overflow).c
    Solución para el problema #173 de Project Euler, overflow

---
+++ C/Project Euler #173 (overflow).c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define TILES 1000000
+
+ /* Solución para el problema #173 de Project Euler
+  * https://projecteuler.net/problem=173       */
+
+int main()
+{
+    int respuesta = 0;
+    // TILES < a < b
+    for (long long int a = 3; a <= TILES; a++){ //a representa el perímetro y el "grueso" de las láminas
+        for (long long int b = 1; b <= (a-2); b++){ //a representa el perímetro y el "grueso" de las láminas
+            if (((a%2 == 0 && b%2 == 0) || (a%2 != 0 && b%2 !=0)) && ((a+b)*(a-b) <= TILES)){ //Expresando a^2 - b^2 como (a+b)(a-b)
+                respuesta++; //a y b deben tener la misma paridad, y no deben exceder la cantidad de casillas.
+                if (a % 1000 == 0) printf ("a: %d  b: %d  Respuesta: %d\n", a, b, respuesta);
+            }
+        }
+    }
+    printf("La respuesta es %d\n", respuesta);
+    return 0;
+}
