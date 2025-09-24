fca6c49635f9bd056769ba98da7c13f43797d232
Santiago Viana <68198881+SantiagodeViana@users.noreply.github.com>
2025-09-24 03:54:03 +0200

    Update isPalindromic.c
    Simplificando función, resolviendo para casos de leading zeros en el medio.

--- C/Funciones/isPalindromic.c
+++ C/Funciones/isPalindromic.c
@@ -1,30 +1,17 @@
-//Función para comprobar si un entero es palíndromo
+//Función usada en Project Euler #125
 
-int isPalindromic(long long int n){
-    int palindrome = 1, i = 0, digs = 0; //Declarando digs como contador de digs para reconocer cuando se parte n. Shift guarda el dígito que se mueve de derecha a izquierda
-    long long int aux = 0, pow = 1, left, right;
-    if (n >= 10){
-        while (pow < n){ //Se mide n
-            pow *= 10;
-            digs++;
-        }
-        pow = 1; //Reiniciando pow
-        while (i < digs/2){ //Se mide parte derecha antes de cortar
-            pow *= 10;
-            i++;
-        }
-        left = n % pow; //Se copia la parte derecha para ir copiando a la izquierda
-        if (digs % 2 != 0) right = n % (pow*10); //Se considera medio de número en casos impares
-        else right = n % pow; //Se recorta la derecha
-        while (pow > 1 && left > 0){ //Se mueve la parte derecha a la izquierda, dígito por dígito
-            while (pow > left) pow /= 10;
-            aux = left / pow; //Se agarra el siguiente dígito
-            while (pow < right) pow *= 10;
-            right += aux * pow; //Se prepara para sumar en la izquierda
-            while (pow > left) pow /= 10;
-            left %= pow;
-        }
-        if (right != n) palindrome = 0; //Si el número es diferente después de los cambios, no es palíndromo
+int isPalindromic(long long int n){ //Función para comprobar si un entero es palíndromo
+    long long int pow = 1, left, right;
+    if (n < 0) return 0;
+    else if (n < 10) return 1;
+    while (n / pow >= 10) pow *= 10; //Midiendo n
+    while (n > 0) {
+        left  = n / pow;
+        right = n % 10;
+        if (left != right) return 0;
+        //Se quitan los extremos en cada iteración
+        n = (n % pow) / 10;
+        pow /= 100;
     }
-    return palindrome;
+     return 1;
 }
