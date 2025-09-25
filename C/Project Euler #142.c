e0ee8942a85fc3277f6761ee67122874dbae672a
Santiago Viana <68198881+SantiagodeViana@users.noreply.github.com>
2025-09-25 22:57:21 +0200

    Create Project Euler #142.c
    Solución para el problema #142 de Project Euler

---
+++ C/Project Euler #142.c
@@ -0,0 +1,53 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+ /* Solución para el problema #142 de Project Euler
+  * https://projecteuler.net/problem=142            */
+
+int main()
+{
+    int x = 3, y = 2, z = 1, i = 1;
+    double raiz; //Evalúa si las expresiones son cuadrados perfecto o no
+    long long int respuesta = 0;
+    while (respuesta == 0){
+        while (x > y){
+            raiz = sqrt(x + y);
+            if (raiz == (int)raiz){
+                raiz = x - y; //Sólo se evalúa 'z' si x y 'y' son válidos
+                while(raiz == (int)raiz && y > z && respuesta == 0){ //Sale del bucle al descartar casos o conseguir respuesta
+                    switch (i) {
+                        case 1:
+                            raiz = sqrt(x + z);
+                            break;
+                        case 2:
+                            raiz = sqrt(x - z);
+                            break;
+                        case 3:
+                            raiz = sqrt(y + z);
+                            break;
+                        case 4:
+                            raiz =  sqrt(y - z);
+                            break;
+                        case 5: //Respuesta conseguida
+                            printf("x:%d y:%d z%d\n", x, y, z);
+                            respuesta =  x + y + z;
+                            break;
+                        default:
+                            z++;
+                            break;
+                    }
+                    i++;
+                }
+                i = 1;
+                z = 1;
+            }
+            y++;
+        }
+        printf("x:%d y:%d z%d\n", x, y, z);
+        y = 2;
+        x++;
+    }
+    printf("La respuesta es %d\n", respuesta);
+    return 0;
+}
