b550858501eac2153a7e98bee4d529b93f71db5f
Santiago Viana <68198881+SantiagodeViana@users.noreply.github.com>
2025-09-30 17:13:25 +0200

    Create Project Euler #172 (por desarrollar).c

---
+++ C/Project Euler #172 (por desarrollar).c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+ /* Solución para el problema #172 de Project Euler
+  * "¿Cuántos números de 18 dígitos n (sin ceros iniciales) hay en los que ningún dígito aparezca más de tres veces en n?"
+  * https://projecteuler.net/problem=172            */
+
+int main()
+{
+    int respuesta = 0;
+
+    printf("La respuesta es %lld\n", respuesta);
+    return 0;
+}
