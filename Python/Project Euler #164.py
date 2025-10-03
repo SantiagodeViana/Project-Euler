5b8bffe62cea2551d3c99e6b8b096869d017169b
Santiago Viana <68198881+SantiagodeViana@users.noreply.github.com>
2025-10-03 23:34:44 +0200

    Create Project Euler #164.py
    Convirtiendo de Project Euler #164 (overflow).c y corrigiendo

---
+++ Python/Project Euler #164.py
@@ -0,0 +1,32 @@
+def threeDigitSum(n: int) -> int:
+    if n < 100: #Descartando números de menos de 3 dígitos
+        return 1
+    else:
+        sum = 0
+        aux2 = n % 10
+        n //= 10
+        aux = n % 10
+        n //= 10
+        while n > 0 and sum <= 9:
+            sum = (n % 10) + aux + aux2
+            aux2 = aux
+            aux = n % 10
+            n //= 10
+    if sum <= 9:
+        return 1
+    else:
+        return 0
+
+
+def main():
+    respuesta = 0
+    for n in range(10_000_000_000_000_000_000, 100_000_000_000_000_000_000):
+        if threeDigitSum(n) == 1:
+            respuesta += 1
+        if (n % 1000000 == 0):
+            print(f"n: {n}  Respuesta:{respuesta}")
+    print(f"La respuesta es: {respuesta}")
+
+
+if __name__ == "__main__":
+    main()
