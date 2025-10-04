08f66bb3a1dc57ce4982598e3173b912c839e74a
Santiago Viana <68198881+SantiagodeViana@users.noreply.github.com>
2025-10-04 23:44:37 +0200

    Update Project Euler #088 (por corregir).c
    Agregando descripción

--- C/Project Euler #088 (por corregir).c
+++ C/Project Euler #088 (por corregir).c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 
 /* Solución para el problema #88 de Project Euler
+ * "¿Cuál es la suma de todos los números mínimos 'producto-suma' para 2 ≤ k ≤ 12000?"
  * https://projecteuler.net/problem=88           */
 
 int main()
