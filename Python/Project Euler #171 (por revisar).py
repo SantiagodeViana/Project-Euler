56ec57133b5b90e3d1ae3555e9f622274d983c6c
Santiago Viana <68198881+SantiagodeViana@users.noreply.github.com>
2025-10-05 17:38:42 +0200

    Update Project Euler #171 (por revisar).py
    Tarda mucho en generar respuesta

--- Python/Project Euler #171 (por revisar).py
+++ Python/Project Euler #171 (por revisar).py
@@ -7,15 +7,18 @@ def squareSum(n):  # Devuelve suma del cuadrado de los dígitos de n
         n //= 10
     return suma
 
-#  /* Solución del problema #171 de Project Euler
-#   * "Encuentra los últimos nueve dígitos de la suma de todos los n, 0 < n < 10^20, tales que f(n) sea un cuadrado perfecto."
-#   * https://projecteuler.net/problem=171                   */
+# Solución del problema #171 de Project Euler
+# "Encuentra los últimos nueve dígitos de la suma de todos los n, 0 < n < 10^20, tales que f(n) sea un cuadrado perfecto."
+# https://projecteuler.net/problem=171
+# Tarda mucho en generar respuesta
 
 def main():
-    i = 1
+   
+    i = 640000000
     square = 0
-    respuesta = 0
+    respuesta = 7302829116635220
     # f(640000000) = 52 Respuesta: 7302829116635220
+    # f(5063000000) = 70 Respuesta: 425636745228940165
     while i <= 100000000000000000000:
         square = squareSum(i)
         if math.isqrt(square) ** 2 == square:  # Comprueba cuadrados perfectos: aquellos cuya raíz sean enteros
