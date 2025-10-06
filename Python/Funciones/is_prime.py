30f6b21621c66206193e51da0312dc77a256fae4
Santiago Viana <68198881+SantiagodeViana@users.noreply.github.com>
2025-10-06 19:31:56 +0200

    Create is_prime.py

---
+++ Python/Funciones/is_prime.py
@@ -0,0 +1,21 @@
+def is_prime(n):
+    if n == 1:
+        return False
+    elif n < 4:
+        return True
+    elif n % 2 == 0:
+        return False
+    elif n < 9:
+        return True
+    elif n % 3 == 0:
+        return False
+    else:
+        r = int(math.isqrt(n))
+        f = 5
+        while f <= r:
+            if n % f == 0:
+                return False
+            elif n % (f + 2) == 0:
+                return False
+            f += 6
+        return True
