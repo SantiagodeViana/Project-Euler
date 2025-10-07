41f5fc38a5499067e1a088eef8b625538bb3ed17
Santiago Viana <68198881+SantiagodeViana@users.noreply.github.com>
2025-10-07 20:16:20 +0200

    Update digCount.c

--- C/Funciones/digCount.c
+++ C/Funciones/digCount.c
@@ -1,10 +1,10 @@
 //Función usada en Project Euler #156
 
 int digCount(int n, int d){ //Devuelve las veces en las que d aparece en n
-    int digs = 0;
+    int count = 0;
     while (n > 0){
-        if (n % 10 == d) digs++;
+        if (n % 10 == d) count++;
         n /= 10;
     }
-    return digs;
+    return count;
 }
