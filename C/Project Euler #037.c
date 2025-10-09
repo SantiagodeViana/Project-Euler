2950c7c71e4fb01ac57e108c6fcd565c5dba0f7f
Santiago Viana <68198881+SantiagodeViana@users.noreply.github.com>
2025-10-09 17:18:43 +0200

    Update Project Euler #037.c
    Criba de Eratóstenes

--- C/Project Euler #037.c
+++ C/Project Euler #037.c
@@ -49,13 +49,13 @@ int right(int n){ //Desde la derecha
     return 0;
 }
 
-int isPrime(int n){//Función para determinar si un numero es primo.    if (n == 1) return 0;
+int isPrime(int n){//Función para determinar si un numero es primo, basada en la Criba de Eratóstenes
     if (n == 1) return 0;
     else if (n < 4) return 1; //Si son menores de 4, son primos
     else if (n % 2 == 0) return 0; //Si son pares, no son primos
     else if (n < 9) return 1; //Si son menores de 9 e impares, son primos
     else if (n % 3 == 0) return 0; //Si son múltiplos de 3, no son primos
-    else{ //Sierva (?) cuyo nombre no recuerdo
+    else{ 
         int r = floor(sqrt(n));
         int f = 5;
         while (f <= r){
