14d6724347016fdda4c8e8ba21e6076a35d7f6ca
Santiago Viana <68198881+SantiagodeViana@users.noreply.github.com>
2025-10-09 18:50:28 +0200

    Update PathSum.java
    Colocando límite arbitrario para reducir tiempo de resolución

--- Java/ProjectEuler082_(No_terminado)/PathSum.java
+++ Java/ProjectEuler082_(No_terminado)/PathSum.java
@@ -5,7 +5,7 @@ public class PathSum {
         for (int i = 0; i < matriz.length - 1; i++){ //Se empiezan todos los caminos
             visitados[i][0] = true;
             valorAcumulado += matriz[i][0];
-            System.out.println("Analizando fila " + i + "...");
+            System.out.println("Empezando desde fila " + i + "...");
             minValorAux(matriz, visitados, 0, 0, sol, valorAcumulado);
             valorAcumulado -= matriz[i][0];
             visitados[i][0] = false;
@@ -19,23 +19,22 @@ public class PathSum {
                 System.out.println("Una posible solución es: " + sol.getSolucion());
             }
         } else {
-            for (int c = 0; c < 3; c++) { // c=0 : Derecha ; c=1 : Arriba; c=2 : Abajo
+            for (int c = 0; c < 3; c++) {
                 if (c == 0 && i > 0 && !visitados[i - 1][nivel]) i--;
                 else if (c == 1 && nivel < matriz.length - 1 && !visitados[i][nivel + 1]) nivel++;
                 else if (c == 2 && i < matriz.length - 1 && !visitados[i + 1][nivel]) i++;
-                //Optimizando con backtracking: aceptando solo pasos con posibles soluciones menores
-                if (!visitados[i][nivel] && (valorAcumulado < sol.getSolucion())){ //((valorAcumulado < sol.getSolucion()) && (valorAcumulado < 351202))
+                //Optimizando: aceptando solo pasos con posibles soluciones menores
+                if (!visitados[i][nivel] && (valorAcumulado < sol.getSolucion()) && (valorAcumulado < 150000)){ //150000 es un límite arbitrario que busca limitar búsquedas y conseguir una solución menor
                     visitados[i][nivel] = true;
                     valorAcumulado += matriz[i][nivel];
                     minValorAux(matriz, visitados, nivel, i, sol, valorAcumulado);
                     visitados[i][nivel] = false;
                     valorAcumulado -= matriz[i][nivel];
-                    if (c == 0) i++;
-                    else if (c == 1) nivel--;
-                    else if (c == 2) i--;
+                    if (c == 0) i++; //c=0: Derecha
+                    else if (c == 1) nivel--; //c=1: Arriba
+                    else if (c == 2) i--; //c=2: Abajo
                 }
             }
         }
     }
 }
-
