ca6f4b9dadfc665680a1ed4685946253188ea54c
Santiago Viana <68198881+SantiagodeViana@users.noreply.github.com>
2025-10-14 15:43:37 +0200

    Update Project Euler #160 (overflow).c
    Agregando descripción

--- C/Project Euler #160 (overflow).c
+++ C/Project Euler #160 (overflow).c
@@ -1,6 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+ /* Solución para el problema #160 de Project Euler
+  * "Para cualquier N, sea f(N) los últimos cinco dígitos antes de los ceros finales en N!.
+  * Encuentra f(1 000 000 000 000)."
+  * https://projecteuler.net/problem=160       */
+
 int factTrail(int n); //n!
 
 int main()
