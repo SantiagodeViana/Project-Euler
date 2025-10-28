2fa8b42c8887941a67023348b69f3d9aedb5c3e9
Santiago Viana <68198881+SantiagodeViana@users.noreply.github.com>
2025-10-28 20:20:34 +0100

    Update Project Euler #069.c
    Limpiando descripción

--- C/Project Euler #069.c
+++ C/Project Euler #069.c
@@ -6,10 +6,7 @@ int isPrime(int n);
 
 /* Solución para el problema #69 de Project Euler
  * "Encuentre el valor de n ≤ 1 000 000 para el que n / φ(n) es un máximo"
- * https://projecteuler.net/problem=69
- * El programa sólo busca el número con la mayor cadena de divisores primos (el número con la mayor cantidad de divisores)
- * En teoría, este es el número que tendría el menor radio de Totient
- * La respuesta es 510510                                                 */
+ * https://projecteuler.net/problem=69                                        */
 
 int main()
 {
