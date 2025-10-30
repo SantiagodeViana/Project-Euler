c00f405c2921f6ef70a0b7e456eee2268fa2b3f9
Santiago Viana <68198881+SantiagodeViana@users.noreply.github.com>
2025-10-30 15:32:59 +0100

    Update Project Euler #077.c

--- C/Project Euler #077.c
+++ C/Project Euler #077.c
@@ -23,7 +23,7 @@ int main()
 }
 
 int isPrime(int n){//Función para determinar si un numero es primo, basada en la Criba de Eratóstenes
-    if (n == 1) return 0; ////*Cambiar a 1 o mantener a conveniencia del programa
+    if (n == 1) return 0; //Cambiar a 1 o mantener a conveniencia del programa
     else if (n < 4) return 1; //Si son menores de 4, son primos
     else if (n % 2 == 0) return 0; //Si son pares, no son primos
     else if (n < 9) return 1; //Si son menores de 9 e impares, son primos
