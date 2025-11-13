ac9088dd6648d322782cdc4dfee666faaa4c3f82
Santiago Viana <68198881+SantiagodeViana@users.noreply.github.com>
2025-11-13 01:07:14 +0100

    Create Project Euler #211 (por corregir).c

---
+++ C/Project Euler #211 (por corregir).c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+int divSquareSum(int n);
+
+int main()
+{
+    int aux, respuesta = 0;
+    for (int i=0; i < 64000000; i++){
+        aux = divSquareSum(i);
+        if (sqrt.(aux) == (int)sqrt.(aux)) respuesta++;
+    }
+    return 0;
+}
+
+int divSquareSum(int n){
+    int sum;
+    while(divSquareSum => 0){
+        sum *= div*div;
+        n /= 10;
+    }
+    return sum;
+}
